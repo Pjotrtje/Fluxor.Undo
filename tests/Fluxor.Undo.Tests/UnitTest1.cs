5d6f5fcbb9085b5b4721952d38107d0e0100c916
Pieter van Sambeek <pietervansambeek@hotmail.com>
2022-12-22 20:51:19 +0100

    Added dummy test

--- tests/Fluxor.Undo.Tests/UnitTest1.cs
+++ tests/Fluxor.Undo.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace Fluxor.Undo.Tests;
 
 public class UnitTest1
@@ -5,5 +7,14 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var x = Undoable.Create(0);
+
+        x.Past.Should().BeEmpty();
+        x.TimeTravelInfo.HasPast.Should().BeFalse();
+        x.TimeTravelInfo.HasNoPast.Should().BeTrue();
+        x.Present.Should().Be(0);
+        x.Future.Should().BeEmpty();
+        x.TimeTravelInfo.HasFuture.Should().BeFalse();
+        x.TimeTravelInfo.HasNoFuture.Should().BeTrue();
     }
 }
