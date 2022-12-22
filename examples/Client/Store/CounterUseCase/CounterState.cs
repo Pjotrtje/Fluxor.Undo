0fab176819ae4ca767f09d12b2da1d2ab8d6afa3
Pieter van Sambeek <pietervansambeek@hotmail.com>
2022-12-22 13:07:16 +0100

    FluxorBlazorWeb.ReduxDevToolsTutorial.Client added

---
+++ examples/Client/Store/CounterUseCase/CounterState.cs
@@ -0,0 +1,16 @@
+﻿using Fluxor;
+
+namespace FluxorBlazorWeb.ReduxDevToolsTutorial.Client.Store.CounterUseCase
+{
+	[FeatureState(Name = "Counter")]
+	public class CounterState
+	{
+		public int ClickCount { get; }
+
+		private CounterState() { }
+		public CounterState(int clickCount)
+		{
+			ClickCount = clickCount;
+		}
+	}
+}
---
+++ examples/Client/Store/FetchDataResultAction.cs
@@ -0,0 +1,15 @@
+﻿using FluxorBlazorWeb.ReduxDevToolsTutorial.Shared;
+using System.Collections.Generic;
+
+namespace FluxorBlazorWeb.ReduxDevToolsTutorial.Client.Store
+{
+	public class FetchDataResultAction
+	{
+		public IEnumerable<WeatherForecast> Forecasts { get; }
+
+		public FetchDataResultAction(IEnumerable<WeatherForecast> forecasts)
+		{
+			Forecasts = forecasts;
+		}
+	}
+}
